3e176cf8ffd3bf3180eaec127ce7a31d4669006e
RISHABH <gguptarishabh82@gmail.com>
2022-03-23 19:03:19 +0530

    Method Calling

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure_Programs_Day_16_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UC3_StackDataStructure stackDS = new UC3_StackDataStructure();
+            stackDS.StackDataStructure();
+
+
+            UC4_QueueDataStructure queueDS = new UC4_QueueDataStructure();
+            queueDS.QueueDataStructure();
+
+            Console.ReadLine();
+        }
+    }
+}
